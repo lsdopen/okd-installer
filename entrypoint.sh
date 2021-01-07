e3b51bb6cb0ad4d14bfb52fff39ff53aba6421b5
Neil White <neil@lsd.co.za>
2021-01-07 17:26:13 +0200

    added destination

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ echo ""
 curl -L -C - https://github.com/openshift/okd/releases/download/4.5.0-0.okd-2020-10-15-235428/openshift-install-linux-$OKD_VERSION.tar.gz -o /srv/okd-installer/openshift-install-linux-$OKD_VERSION.tar.gz
 echo "extracting openshift-install-linux-$OKD_VERSION.tar.gz ..."
 echo ""
-tar zxvf /srv/okd-installer/openshift-install-linux-$OKD_VERSION.tar.gz
+tar zxvf /srv/okd-installer/openshift-install-linux-$OKD_VERSION.tar.gz -C /srv/okd-installer/
